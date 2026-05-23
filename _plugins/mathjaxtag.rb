d4817ffe9fc9db182f827b7ed6299f6e5f0bd518
Nate Berkopec <nate.berkopec@gmail.com>
2026-05-23 11:28:55 +0900

    Publish thoughts on LLMs in 2026

--- _plugins/mathjaxtag.rb
+++ _plugins/mathjaxtag.rb
@@ -4,18 +4,20 @@ module Jekyll
       '<script type="math/tex; mode=display">'
     end
   end
-class MathJaxInlineTag < Liquid::Tag
+
+  class MathJaxInlineTag < Liquid::Tag
     def render(context)
       '<script type="math/tex">'
     end
   end
-class MathJaxEndTag < Liquid::Tag
+
+  class MathJaxEndTag < Liquid::Tag
     def render(context)
       '</script>'
     end
   end
 end
- 
+
 Liquid::Template.register_tag('math', Jekyll::MathJaxBlockTag)
 Liquid::Template.register_tag('m', Jekyll::MathJaxInlineTag)
 Liquid::Template.register_tag('endmath', Jekyll::MathJaxEndTag)
